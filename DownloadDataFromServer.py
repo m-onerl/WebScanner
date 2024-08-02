21d1cdfb9f93d1ec7c3a4f40bd1a002312f89ac8
m0nerl <monerl2525@gmail.com>
2024-08-02 21:54:08 +0200

    changing to find a to values, important module with function to look for a most value from some json file

--- DownloadDataFromServer.py
+++ DownloadDataFromServer.py
@@ -1,6 +1,11 @@
 """pobieranie danych z serwera"""
 """
-
+def get_keys_with_top_values(my_dict):
+    return[   
+        key
+        for (key, value) in my_dict.items()
+        if value == max(my_dict.values())
+    ]
 """
 
 import requests
@@ -8,12 +13,7 @@ import json
 
 r = requests.get("https://jsonplaceholder.typicode.com/todos")
 
-
-try:
-    tasks = r.json()
-except json.decoder.JSONDecodeError:
-    print("Błąd dekodowania JSON")
-else:
+def count_task_frequency(tasks):
     taskFrequency = {}
     for entry in tasks:
         if(entry["completed"] == True):
@@ -21,4 +21,30 @@ else:
                 taskFrequency[entry["userId"]] += 1
             else:
                 taskFrequency[entry["userId"]] = 1
-print(taskFrequency)
+    return taskFrequency
+
+def get_keys_with_top_values(my_dict):
+    return[   
+        key
+        for (key, value) in my_dict.items()
+        if value == max(my_dict.values())
+    ]
+
+def user_with_most_completed(taskFrequency):
+    userWithMost = []
+    maxAmount = max(taskFrequency.values())
+    for userId, numberOfCompleted in taskFrequency.items():
+        if (numberOfCompleted == maxAmount):
+            userWithMost.append(userId)
+    return userWithMost
+
+try:
+    tasks = r.json()
+except json.decoder.JSONDecodeError:
+    print("Błąd dekodowania JSON")
+else:
+    taskFrequency = count_task_frequency(tasks)
+    UsersWithTopCompleted = (user_with_most_completed(taskFrequency))
+
+    print("Wreczamy nagrody dla użytkowników z id:",UsersWithTopCompleted)
+    print(get_keys_with_top_values(taskFrequency))
